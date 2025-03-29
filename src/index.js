740d64a40dfa336e981b7fdad8ba73d20af27326
daytoney3971 <daytoney3971@gmail.com>
2025-03-29 23:36:49 +0800

    add React components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
