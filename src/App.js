085ff4d9ad426672d55f7ada0bf8388386640067
daytoney3971 <daytoney3971@gmail.com>
2025-03-30 00:22:55 +0800

    fix: correct App.js content and package.json format

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-﻿import React from 'react';
-import { Box, Container, Typography } from '@mui/material';
+import React from "react";
+import { Box, Container, Typography } from "@mui/material";
 
 function App() {
   return (
